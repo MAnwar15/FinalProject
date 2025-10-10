bff03fc651d7ff71105155de93960941898abe07
HUSSAEN <hussaenalaa8@gmail.com>
2025-10-10 19:39:33 +0300

    eating
    thats how the player will eat
    u will find :
    2 scripts
    2 prefabs
    1 soundeffect
    1 material

--- Assets/eat/NewMonoBehaviourScript.cs
+++ Assets/eat/NewMonoBehaviourScript.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Collider _collider;
+
     void Start()
     {
-        
+        _collider = GetComponent<Collider>();
+        if (_collider != null)
+            _collider.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        Consumable consumable = other.GetComponent<Consumable>();
+        if (consumable != null && !consumable.isFinished)
+        {
+            consumable.Consume();
+        }
     }
 }
