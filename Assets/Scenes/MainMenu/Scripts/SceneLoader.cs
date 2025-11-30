e30706884e7b72fbc2906b1d393bad694b0af055
HUSSAEN <hussaenalaa8@gmail.com>
2025-11-30 23:00:29 +0200

    SENCE LOADER

---
+++ Assets/Scenes/MainMenu/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName;   // اسم السين اللي هيروح لها المستخدم
+
+    public void LoadMyScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
