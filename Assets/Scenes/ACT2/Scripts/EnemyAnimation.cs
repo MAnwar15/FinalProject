8d1b8effbaf15973bbf6d1ec4374f1a39a1b4507
HUSSAEN <hussaenalaa8@gmail.com>
2025-12-17 19:46:42 +0200

    update

---
+++ Assets/Scenes/ACT2/Scripts/EnemyAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // مثال: العدو يمشي دايماً
+        anim.SetBool("isWalking", true);
+    }
+}
